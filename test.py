e83dfa53f98ff8fc3adda3e8bdfac3843e6d82a9
Mark Attar <124407382+markattarcolgate64@users.noreply.github.com>
2025-12-28 18:03:36 -0500

    runpod commits

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 from openrouter import OpenRouter
 import os
 import dotenv
-from run_persona_questions import THINK_TOKEN, call_openrouter_api, judge_inference_openrouter_batch
+import json
+from run_persona_questions import WRITE_FP, THINK_TOKEN, call_openrouter_api, judge_inference_openrouter_batch
 dotenv.load_dotenv()
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 e = "be a teacher needing classroom material, or someone feeling seasonal depression. But since they didn\'t specify, I\'ll keep it universally warm but simple. \n\nFinal check: All lines directly about spring phenomena. Zero personal commentary. Exactly what they asked for. Done.\n</think>\n\nSnowmelt sings on frozen streams,"
@@ -15,6 +16,9 @@ def main():
     idx = e.find(THINK_TOKEN)
     print(idx, len(e))
     print(e[idx+len(THINK_TOKEN):])
+
+    with open(WRITE_FP, 'w') as f:
+        json.dump(e, f, indent=2)
 
 
 if __name__ == "__main__":
